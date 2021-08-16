732fe3d5b3770ddc8225842c33c8b0ff8c47b380
Md Tasquil Noori <mtnoori40@gmail.com>
2021-08-16 15:23:31 +0530

    Class Changes

---
+++ GigMusicHub/Models/ApplicationUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace GigMusicHub.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            return userIdentity;
+        }
+    }
+   
+}
---
+++ GigMusicHub/Models/Gig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigMusicHub.Models
+{
+    public class Gig 
+    {
+        public int Id { get; set; }
+        public ApplicationUser Artist { get; set; }
+        public DateTime DateTime { get; set; }
+        public string venue { get; set; }
+        public Genre Genre { get; set; }
+
+
+    }
+   
+
+}

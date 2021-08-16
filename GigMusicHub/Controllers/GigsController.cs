8dfc7611c7cf8bb79b81e418e04052e8f440800c
Md Tasquil Noori <mtnoori40@gmail.com>
2021-08-16 21:12:31 +0530

    Gigs Feature added

---
+++ GigMusicHub/Controllers/GigsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GigMusicHub.Models;
+using GigMusicHub.ViewModels;
+
+namespace GigMusicHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext _context;
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var ViewModel = new GigForViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(ViewModel);
+        }
+    }
+}
---
+++ GigMusicHub/ViewModels/GigForViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GigMusicHub.Models;
+
+namespace GigMusicHub.ViewModels
+{
+    public class GigForViewModel
+    {
+        public string venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}

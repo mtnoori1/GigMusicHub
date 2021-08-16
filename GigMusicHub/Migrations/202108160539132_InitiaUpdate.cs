5431aab7ed2deeb3232e85686a84c9a4aca9b1d7
Md Tasquil Noori <mtnoori40@gmail.com>
2021-08-16 12:27:51 +0530

    Enabled Migrations

---
+++ GigMusicHub/Migrations/202108160539132_InitiaUpdate.cs
@@ -0,0 +1,16 @@
+﻿namespace GigMusicHub.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitiaUpdate : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
